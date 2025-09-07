9d331307810080f3f57335edbfa2cd2818163144
shreyabyte <shreya.gupta2326@gmail.com>
2025-09-08 00:47:06 +0530

    landmark/fingertip tracking mapped (unversal base done)

--- main.py
+++ main.py
@@ -15,7 +15,16 @@ while True:
 
     results = hands.process(imgRGB)
 
-    cv2.imshow("Image", img)
+    if results.multi_hand_landmarks:
+         for hand_landmarks in results.multi_hand_landmarks:
+              mpDraw.draw_landmarks(img, hand_landmarks, mpHands.HAND_CONNECTIONS)
+
+              for id,lm in enumerate(hand_landmarks.landmark):
+                   h,w,c= img.shape
+                   cx,cy=int(lm.x * w), int(lm.y * h)
+                   
+
+    cv2.imshow("Webcam Feed", img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
          break
